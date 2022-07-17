b9a942b7100eddebbf408d00f72deb0f0a3cf7b4
starkovAE <swix456@gmail.com>
2022-07-17 13:13:32 +0500

    Initial Commit

---
+++ NoteBookNew/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NoteBookNew
+//
+//  Created by Александр Старков on 17.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
